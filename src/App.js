28cfb74b989bbe599a02fe347db8161ed300393f
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-19 02:29:16 +0300

    useState (Hook)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState("dev");
   const showEmployees = true;
   return (
     <div className="App">
 
       {showEmployees ? (
       <>
+      <input type="text" onChange={(e) => {
+        console.log(e.target.value);
+        setRole(e.target.value);
+      }}
+      />
           <Employee name="Hashim" role="Intern"/>
-          <Employee name="Abby"/>
+          <Employee name="Abby" role={role}/>
           <Employee name="John"/>
       </>
       ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,7 +3,6 @@ function Employee(props){
     <>
         <h3>Employee {props.name}</h3>
         <p>{props.role ? props.role : "No role"}</p>
-        {props.role ? <p class="role">{props.role}</p> : <p class="norole">No role</p>}
     </>
     )
 }
