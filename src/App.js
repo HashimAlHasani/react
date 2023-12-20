f77c76e4e2b03e3612af472da68c965c6cd82c3f
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-20 03:15:37 +0300

    Map Through State Array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: "Erwin Smith", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg",
+      },
+      {
+        name: "Levi Ackerman", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19564637/pexels-photo-19564637/free-photo-of-levi.jpeg",
+      },
+      {
+        name: "Eren Yaeger", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19564649/pexels-photo-19564649.jpeg",
+      },
+      {
+        name: "Mikasa Ackerman", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564661/pexels-photo-19564661/free-photo-of-mikasa.jpeg",
+      },
+      {
+        name: "Armin Arlert", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564668/pexels-photo-19564668/free-photo-of-armin.jpeg",
+      },
+      {
+        name: "Hange  Zoë", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564675/pexels-photo-19564675/free-photo-of-hange.jpeg",
+      },
+    ]
+  );
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -16,15 +52,16 @@ function App() {
       }}
       />
       <div className='flex flex-wrap justify-center'>
-          <Employee name="Hashim" role="Intern" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="Abby" role={role} img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="John" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>          
-          <Employee name="Hashim" role="Intern" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="Abby" role={role} img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="John" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
-          <Employee name="Hashim" role="Intern" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="Abby" role={role} img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-          <Employee name="John" img="https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
+          {employees.map((employee) => {
+            return (
+              <Employee 
+              key={uuidv4()}
+              name={employee.name} 
+              role={employee.role} 
+              img={employee.img}
+              />
+            );
+          })}
       </div>
       </>
       ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -16,7 +16,7 @@ function Employee(props){
                 </p>
                 </div>
                 <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Message
+                    Update
                 </button>
             </div>
         </div>
