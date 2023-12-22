7a0995dc5be0a16f6da00eec674f8469b0d36cbd
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-23 02:42:52 +0300

    Introduce pages and children props

---
+++ src/pages/Employees.js
@@ -0,0 +1,109 @@
+import '../index.css';
+import Employee from '../components/Employee';
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import AddEmployee from '../components/AddEmployee';
+import EditEmployee from '../components/EditEmployee';
+import Header from '../components/Header';
+
+function Employees() {
+  const [employees, setEmployees] = useState(
+    [
+      {
+        id: 1,
+        name: "Erwin Smith", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19554075/pexels-photo-19554075.jpeg",
+      },
+      {
+        id: 2,
+        name: "Levi Ackerman", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19564637/pexels-photo-19564637/free-photo-of-levi.jpeg",
+      },
+      {
+        id: 3,
+        name: "Eren Jaeger", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564649/pexels-photo-19564649.jpeg",
+      },
+      {
+        id: 4,
+        name: "Mikasa Ackerman", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564661/pexels-photo-19564661/free-photo-of-mikasa.jpeg",
+      },
+      {
+        id: 5,
+        name: "Armin Arlert", 
+        role: "Survey Corps", 
+        img: "https://images.pexels.com/photos/19564668/pexels-photo-19564668/free-photo-of-armin.jpeg",
+      },
+      {
+        id: 6,
+        name: "Hange Zoë", 
+        role: "Commander", 
+        img: "https://images.pexels.com/photos/19564675/pexels-photo-19564675/free-photo-of-hange.jpeg",
+      },
+    ]
+  );
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee) => {
+      if(id == employee.id){
+        return {...employee, name: newName, role: newRole};
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img){
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    }
+    setEmployees([...employees, newEmployee]);
+  }
+
+  const showEmployees = true;
+  return (
+    <div className="App bg-gray-300 min-h-screen">
+      {showEmployees ? (
+      <>
+      <div className='flex flex-wrap justify-center my-2'>
+          {employees.map((employee) => {
+            const editEmployee = (
+            <EditEmployee 
+              id={employee.id}
+              name={employee.name} 
+              role={employee.role} 
+              updateEmployee={updateEmployee}
+            />
+            );
+            return (
+              <Employee 
+                key={employee.id}
+                id={employee.id}
+                name={employee.name} 
+                role={employee.role} 
+                img={employee.img}
+                editEmployee={editEmployee}
+              />
+            );
+          })}
+      </div>
+      <AddEmployee newEmployee={newEmployee}/>
+      </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
+      
+    </div>
+  );
+}
+
+export default Employees;
