dcf1cd93290bfd567fb22e65e30e693a63fce217
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-26 18:54:11 +0300

    Fetch an API to Display on Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './index.css';
 import Employees from './pages/Employees';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Customers from './pages/Customers';
-import Dictionary from './components/Dictionary';
+import Dictionary from './pages/Dictionary';
+import Definition from './pages/Definition';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/employees' element={<Employees/>} />
         <Route path='/customers' element={<Customers/>} />
         <Route path='/dictionary' element={<Dictionary/>} />
+        <Route path='/definition' element={<Definition/>} />
       </Routes>
     </Header>
   </BrowserRouter>
