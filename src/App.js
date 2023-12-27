5220799163ca54e13de58686fd89b04c86312102
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-28 02:06:57 +0300

    404s

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Customers from './pages/Customers';
 import Dictionary from './pages/Dictionary';
 import Definition from './pages/Definition';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path='/customers' element={<Customers/>} />
         <Route path='/dictionary' element={<Dictionary/>} />
         <Route path='/definition/:search' element={<Definition/>} />
+        <Route path='/404' element={<NotFound/>} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </Header>
   </BrowserRouter>
--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,30 +1,51 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import {v4 as uuidv4} from 'uuid';
+import NotFound from "../components/NotFound";
 
 export default function Definition(){
     const [word, setWord] = useState();
+    const [notFound, setNotFound] = useState(false);
     let { search } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
-            .then((response) => response.json())
+            .then((response) => {
+                if(response.status === 404){
+                    setNotFound(true);
+                }
+                return response.json();
+            })
             .then((data) => {
                 setWord(data[0].meanings);
             });
     }, []);
 
+    if(notFound === true) {
+        return (
+            <>
+                <NotFound/>
+                <Link to='/dictionary'>Search Another</Link>
+            </>
+        );
+    }
+
     return (
         <>
-        <h1>Here is a definition:</h1>
-        {word ? word.map((meaning) => {
-            return (
-            <p key={uuidv4()}>
-                {meaning.partOfSpeech + ': '}
-                {meaning.definitions[0].definition}
-            </p>
-            )
-        }): null}
+        {word ? (
+            <>
+                <h1>Here is a definition:</h1>
+                {word.map((meaning) => {
+                    return (
+                        <p key={uuidv4()}>
+                            {meaning.partOfSpeech + ': '}
+                            {meaning.definitions[0].definition}
+                        </p>
+                    );
+                })} 
+            </>
+        ) : null}
         </>
-    )
+    );
 }
