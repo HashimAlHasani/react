5e1bd7457f8b86ee2ea5138e75f288a225de6f54
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-03 03:44:31 +0300

    Consume Backend API

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Customers(){
-    return <h1>Hello There!</h1>;
+    const [customers, setCustomers] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/customers/')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setCustomers(data.customers);
+        });
+    }, []);
+    return (
+        <>
+            <h1>Here are our customers:</h1>
+            {customers ? customers.map((customer) => {
+                return <p>{customer.name}</p>;
+            }) : null}
+        </>
+    );
 }
