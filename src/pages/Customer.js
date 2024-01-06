2f5c902f404a5345891b750c42bf7d24eca0f8d7
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-06 03:15:48 +0300

    CRUD front-end

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -7,7 +7,7 @@ export default function Customer(){
     const { id } = useParams();
     const [customer, setCustomer] = useState();
     const [notFound, setNotFound] = useState();
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
     
     useEffect(() => {
         const url = baseUrl + 'api/customers/' + id;
@@ -23,6 +23,7 @@ export default function Customer(){
             setCustomer(data.customer);
         });
     }, []);
+
     return (
         <>
         {notFound ? <p> The customer with id {id} was not found</p> : null}
@@ -32,6 +33,23 @@ export default function Customer(){
                 <p>{customer.name}</p>
                 <p>{customer.industry}</p>
             </div> : null}
+            <button onClick={() => {
+                const url = baseUrl + '/api/customers/' + id;
+                fetch(url, {
+                    method: 'DELETE', 
+                    headers: {
+                        'Content-Type': 'application/json'
+                },
+            })
+                .then((response) => {
+                    if(!response.ok){
+                        throw new Error('Something went wrong')
+                    }
+                    navigate('/customers');
+                })
+                .catch((e) => console.log(e));
+            }}>Delete</button>
+            <br/>
             <Link to="/customers">Go Back</Link>
         </>
     )
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import {Link} from 'react-router-dom';
 import { baseUrl } from "../shared";
+import AddCustomer from "../components/AddCustomer";
 
 export default function Customers(){
     const [customers, setCustomers] = useState();
+    const [show, setShow] = useState(false);
+
+    function toggleShow(){
+        setShow(!show);
+    }
 
     useEffect(() => {
         const url = baseUrl + 'api/customers/'
@@ -13,16 +19,45 @@ export default function Customers(){
             setCustomers(data.customers);
         });
     }, []);
+
+    function newCustomer(name, industry){
+        const data = {name: name, industry: industry};
+        const url = baseUrl + 'api/customers/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error('Something went wrong!')
+            }
+            return response.json();
+        }).then((data) => {
+            toggleShow();
+            setCustomers([...customers, data.customer]);
+        }).catch((e) => console.log(e));
+    }
+
     return (
         <>
             <h1>Here are our customers:</h1>
-            {customers ? customers.map((customer) => {
-                return (
-                <p>
-                    <Link to={"/customers/" + customer.id}>{customer.name}</Link>
-                </p>
-                );
-            }) : null}
+            <ul>
+                {customers ? customers.map((customer) => {
+                    return (
+                    <li key={customer.id}>
+                        <Link to={"/customers/" + customer.id}>
+                            {customer.name}
+                        </Link>
+                    </li>
+                    );
+                }) : null}
+            </ul>
+            <AddCustomer 
+            newCustomer={newCustomer} 
+            show={show} 
+            toggleShow={toggleShow}/>
         </>
     );
 }
