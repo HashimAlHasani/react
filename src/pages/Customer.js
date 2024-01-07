547962ea000406d977d43e06f2d59def0496410d
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-08 00:50:56 +0300

    Tailwind CSS Form and Button Styling

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -46,7 +46,8 @@ export default function Customer() {
       });
   }, []);
 
-  function updateCustomer() {
+  function updateCustomer(e) {
+    e.preventDefault();
     const url = baseUrl + "api/customers/" + id;
     fetch(url, {
       method: "POST",
@@ -68,71 +69,108 @@ export default function Customer() {
   }
 
   return (
-    <>
+    <div className="p-3">
       {notFound ? <p> The customer with id {id} was not found</p> : null}
       {customer ? (
         <div>
-          <p className="m-2 block px-2">ID: {tempCustomer.id}</p>
-          <input
-            className="m-2 block px-2"
-            type="text"
-            value={tempCustomer.name}
-            onChange={(e) => {
-              setChanged(true);
-              setTempCustomer({ ...tempCustomer, name: e.target.value });
-            }}
-          />
-          <input
-            className="m-2 block px-2"
-            type="text"
-            value={tempCustomer.industry}
-            onChange={(e) => {
-              setChanged(true);
-              setTempCustomer({ ...tempCustomer, industry: e.target.value });
-            }}
-          />
-          {changed ? (
-            <>
-              <button
-                className="m-2"
-                onClick={(e) => {
-                  setTempCustomer({ ...customer });
-                  setChanged(false);
-                }}
-              >
-                Cancel
-              </button>
-              <button className="m-2" onClick={updateCustomer}>
-                Save
-              </button>
-            </>
-          ) : null}
-          <button
-            onClick={() => {
-              const url = baseUrl + "/api/customers/" + id;
-              fetch(url, {
-                method: "DELETE",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              })
-                .then((response) => {
-                  if (!response.ok) {
-                    throw new Error("Something went wrong");
-                  }
-                  setError(undefined);
-                  navigate("/customers");
-                })
-                .catch((e) => setError(e.message));
-            }}
+          <form
+            className="w-full max-w-sm"
+            id="customer"
+            onSubmit={updateCustomer}
           >
-            Delete
-          </button>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/4">
+                <label for="name">Name</label>
+              </div>
+              <div className="md:w-3/4">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="text"
+                  id="name"
+                  value={tempCustomer.name}
+                  onChange={(e) => {
+                    setChanged(true);
+                    setTempCustomer({ ...tempCustomer, name: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/4">
+                <label for="industry">Industry</label>
+              </div>
+              <div className="md:w-3/4">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="industry"
+                  type="text"
+                  value={tempCustomer.industry}
+                  onChange={(e) => {
+                    setChanged(true);
+                    setTempCustomer({
+                      ...tempCustomer,
+                      industry: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+          </form>
+          {changed ? (
+            <div className="mb-2">
+              <>
+                <button
+                  className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 mr-2 rounded"
+                  onClick={(e) => {
+                    setTempCustomer({ ...customer });
+                    setChanged(false);
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                  form="customer"
+                >
+                  Save
+                </button>
+              </>
+            </div>
+          ) : null}
+          <div>
+            <button
+              className="bg-slate-800 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                const url = baseUrl + "/api/customers/" + id;
+                fetch(url, {
+                  method: "DELETE",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                })
+                  .then((response) => {
+                    if (!response.ok) {
+                      throw new Error("Something went wrong");
+                    }
+                    setError(undefined);
+                    navigate("/customers");
+                  })
+                  .catch((e) => setError(e.message));
+              }}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       ) : null}
       {error ? <p>{error}</p> : null}
       <br />
-      <Link to="/customers">Go Back</Link>
-    </>
+      <Link to="/customers">
+        <button className="no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+          {" "}
+          ← Go Back{" "}
+        </button>
+      </Link>
+    </div>
   );
 }
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -46,17 +46,21 @@ export default function Customers() {
   return (
     <>
       <h1>Here are our customers:</h1>
-      <ul>
-        {customers
-          ? customers.map((customer) => {
-              return (
-                <li key={customer.id}>
-                  <Link to={"/customers/" + customer.id}>{customer.name}</Link>
-                </li>
-              );
-            })
-          : null}
-      </ul>
+
+      {customers
+        ? customers.map((customer) => {
+            return (
+              <div className="m-2" key={customer.id}>
+                <Link to={"/customers/" + customer.id}>
+                  <button className="no-underline bg-slate-800 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded">
+                    {customer.name}
+                  </button>
+                </Link>
+              </div>
+            );
+          })
+        : null}
+
       <AddCustomer
         newCustomer={newCustomer}
         show={show}
