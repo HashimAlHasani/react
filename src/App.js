8425311810ccccc2aa9292ece03c052aab05e30b
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-09 03:00:46 +0300

    Auth Refresh Tokens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import "./index.css";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Employees from "./pages/Employees";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Customers from "./pages/Customers";
@@ -9,10 +9,40 @@ import Dictionary from "./pages/Dictionary";
 import Definition from "./pages/Definition";
 import NotFound from "./components/NotFound";
 import Login from "./pages/Login";
+import { baseUrl } from "./shared";
 
 export const LoginContext = createContext();
 
 export default function App() {
+  useEffect(() => {
+    function refreshTokens() {
+      if (localStorage.refresh) {
+        const url = baseUrl + "api/token/refresh/";
+
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          }),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            localStorage.access = data.access;
+            localStorage.refresh = data.refresh;
+            setLoggedIn(true);
+          });
+      }
+    }
+    const minute = 1000 * 60;
+    refreshTokens();
+    setInterval(refreshTokens, minute * 3);
+  }, []);
+
   const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
 
   function changeLoggedIn(value) {
